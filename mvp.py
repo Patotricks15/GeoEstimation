daf7d3827c0a545aa6e7ae3772d6adc8bbdf76d1
Patrick Gomes <66369808+Patotricks15@users.noreply.github.com>
2022-08-03 12:50:27 -0300

    Update mvp.py

--- mvp.py
+++ mvp.py
@@ -345,8 +345,8 @@ st.markdown('## Análise no Estado')
 if st.button('Exibir tabela (Estado)'):
     #st.text('visualizar')
     #st.text(state_df.columns)
-    geo_state_dataframe(state_df[['name_muni', 'abbrev_state', 'populacao', 'pib', 'IDH', 'pib_per_capita','app', 'soma', 'max']+app])
-    st.dataframe()
+    geo_state_dataframe = state_df[['name_muni', 'abbrev_state', 'populacao', 'pib', 'IDH', 'pib_per_capita','app', 'soma', 'max']+app]
+    st.dataframe(geo_state_dataframe)
 if st.button('Exibir mapa (Estado)'):
     for row, value in app_color_dict.iterrows():
         st.text(value['app'])
