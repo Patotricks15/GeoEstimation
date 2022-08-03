1c95c6252fcaf2cb8308e41afdb77324c2fd5aa4
Patrick Gomes <66369808+Patotricks15@users.noreply.github.com>
2022-08-03 14:05:59 -0300

    Update mvp.py

--- mvp.py
+++ mvp.py
@@ -345,11 +345,11 @@ st.markdown('## Análise no Estado')
 if st.button('Exibir tabela (Estado)'):
     #st.text('visualizar')
     #st.text(state_df.columns)[['name_muni', 'abbrev_state', 'populacao', 'pib', 'IDH', 'pib_per_capita','app', 'soma', 'max']+app]
-    geo_state_dataframe = state_df
+    geo_state_dataframe = state_df[['name_muni', 'abbrev_state', 'populacao', 'pib', 'IDH', 'pib_per_capita','app']+app]
     try:
-      st.dataframe(geo_state_dataframe[['name_muni', 'abbrev_state', 'populacao', 'pib', 'IDH', 'pib_per_capita','app', 'soma', 'max']+app])
+      st.dataframe(geo_state_dataframe)
     except:
-      st.write(geo_state_dataframe[['name_muni', 'abbrev_state', 'populacao', 'pib', 'IDH', 'pib_per_capita','app', 'soma', 'max']+app])
+      st.write(geo_state_dataframe)
 if st.button('Exibir mapa (Estado)'):
     for row, value in app_color_dict.iterrows():
         st.text(value['app'])
